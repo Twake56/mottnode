89015e649fec0f24210ac441485db9ac822e6937
Trevor Wakeman <trevor.wakeman@colorado.edu>
2017-03-24 12:46:39 -0600

    Testing if heroku is synced with github

--- index.js
+++ index.js
@@ -243,5 +243,5 @@ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 */
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is starting on port', app.get('port'));
 });
